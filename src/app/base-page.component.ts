0a25abf158c49c2d547a2d1108c263e4a412a808
KpryKorn <roffinisacha@gmail.com>
2025-08-03 10:52:16 +0200

    feat: refactor layout structure and implement base page component

---
+++ src/app/base-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: '',
+  host: {
+    class: 'flex-grow flex',
+  },
+})
+export abstract class BasePageComponent {}
--- src/app/components/pages/page-not-found/page-not-found.component.ts
+++ src/app/components/pages/page-not-found/page-not-found.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { BasePageComponent } from '../../../base-page.component';
 
 @Component({
   selector: 'app-page-not-found',
   imports: [],
   templateUrl: './page-not-found.component.html',
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent extends BasePageComponent {
   private readonly router = inject(Router);
   private readonly location = inject(Location);
 
